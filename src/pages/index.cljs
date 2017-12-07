4896dd2c7e663cdd3c66f27a5a9ed6c7bfe7db04
David Meister <thedavidmeister@gmail.com>
2017-12-07 13:19:06 +1100

    wip on embedding editor

--- src/pages/index.cljs
+++ src/pages/index.cljs
@@ -1,9 +1,12 @@
 (ns ^{:hoplon/page "index.html"} pages.index
  (:require
   [hoplon.core :as h]
-  hoplon.jquery))
+  [javelin.core :as j]
+  hoplon.jquery
+  ckeditor.lib))
 
 (h/html
  (h/head)
  (h/body
-  (h/div "foo")))
+  (j/with-let [el (h/div "foo")]
+   (js/BalloonEditor.create el))))
